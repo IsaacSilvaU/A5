7bb9817382b35cede6c44fa80ac212200da5d608
Insu24 <isaacsilva1107@gmail.com>
2022-03-21 20:11:03 -0500

    Subida de proyecto

---
+++ src/test/java/com/ADA/Actividad5/Actividad5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ADA.Actividad5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
